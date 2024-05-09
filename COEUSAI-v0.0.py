99d5ebae95309e48f974ba23437633e9d176b879
Pva123 <49602493+Pva123@users.noreply.github.com>
2024-05-09 17:49:39 -0400

    Create COEUSAI-v0.0.py

---
+++ COEUSAI-v0.0.py
@@ -0,0 +1,10 @@
+print("Coeus AI")
+print("________")
+user_input = input("Welcome to Coeus AI, how can I assist you?")
+while (user_input):
+    print("I will for sure try to implement everyting as you mentioned.")
+    print("Loading...")
+    print("Here is the final result!")
+    user_input = input("How else can I help you today?")
+
+
