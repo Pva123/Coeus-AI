0becc362174a9f5ef4f6a7c53ff5080476f0fb5d
Pva123 <49602493+Pva123@users.noreply.github.com>
2024-07-31 12:10:11 -0400

    new data (i dont know if its right or not)

---
+++ android_v0.1/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:iphone_v01/Home%20Page/home.dart';
+import 'package:iphone_v01/signin/signup/signup.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
+    );
+  }
+}
