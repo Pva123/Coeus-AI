e9f6f1a422f064b8310eee340b77e6ec2b890a17
Pva123 <49602493+Pva123@users.noreply.github.com>
2024-08-03 21:24:45 -0400

    Progrees bar updates, and clock and top bar corrected

--- iphone_v01/lib/Home Page/home.dart
+++ iphone_v01/lib/Home Page/home.dart
@@ -10,42 +10,77 @@ class HomePage extends StatelessWidget {
         children: [
           
           // Date/Time Section
-
+          
           Positioned(
             top: 50,
             left: 20,
+            right: 20, // Ensure the bar spans the width of the screen with some padding
             child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              width: double.infinity, // Ensures the container takes the full width
+              height: 40, // Set a fixed height for the container
+              padding: EdgeInsets.symmetric(horizontal: 16),
               decoration: BoxDecoration(
-                color: Color.fromARGB(255, 255, 123, 0),
-                borderRadius: BorderRadius.circular(30),
+                color: Color.fromARGB(255, 95, 104, 119),
+                borderRadius: BorderRadius.circular(30), // Higher value for more rounded corners
               ),
-              child: Text(
-                'Jun 12, Friday',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+              child: Center(
+                child: Text(
+                  '12:45',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 50,
-            right: 30,
-            child: Text(
-              '12:45',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+              )
             ),
           ),
 
-          // Navigation Tabs
 
-          
+          // Positioned(
+          //   top: 50,
+          //   left: 20,
+          //   right: 20, // Ensure the bar spans the width of the screen with some padding
+          //   child: Container(
+          //     width: double.infinity, // Ensures the container takes the full width
+          //     height: 60, // Adjusted height to accommodate both time and date
+          //     padding: EdgeInsets.symmetric(horizontal: 16),
+          //     decoration: BoxDecoration(
+          //       color: Color.fromARGB(255, 95, 104, 119),
+          //       borderRadius: BorderRadius.circular(30), // Higher value for more rounded corners
+          //     ),
+          //     child: Center(
+          //       child: Column(
+          //         mainAxisAlignment: MainAxisAlignment.center,
+          //         children: [
+          //           Text(
+          //             DateFormat('HH:mm').format(DateTime.now()), // Display current time
+          //             style: TextStyle(
+          //               color: Colors.white,
+          //               fontSize: 18,
+          //               fontWeight: FontWeight.bold,
+          //             ),
+          //           ),
+          //           SizedBox(height: 4), // Space between time and date
+          //           Text(
+          //             DateFormat('EEE, MMM d').format(DateTime.now()), // Display current date
+          //             style: TextStyle(
+          //               color: Colors.white70,
+          //               fontSize: 12,
+          //             ),
+          //           ),
+          //         ],
+          //       ),
+          //     ),
+          //   ),
+          // ),
+
+
+
+
+          // Navigation Tabs          
+
+
 
           Positioned(
             top: 110,
@@ -98,6 +133,59 @@ class HomePage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
 
+                // In-Progress Tasks
+
+                Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(100, 34, 255, 0),
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                      color: Color.fromARGB(100, 34, 255, 0), // 51 is approximately 20% of 255, for similar 20% opacity
+                      blurRadius: 20.0,
+                      spreadRadius: 1.0,
+                      offset: Offset(0, 4),
+                     ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'In Progress',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Divider(color: const Color.fromARGB(255, 255, 255, 255)), // Divider
+                      SizedBox(height: 8),
+
+                      // Placeholder for in progress items
+                      Container(
+                        height: 60,
+                        decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Color.fromARGB(139, 255, 255, 255),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2), // Shadow color with opacity
+                            offset: Offset(0, 4), // Horizontal and vertical offset
+                            blurRadius: 8.0, // Blur radius
+                            spreadRadius: 0, // Spread radius
+                          ),
+                        ],
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+
+
                 // Upcoming Tasks
                 Container(
                   margin: EdgeInsets.only(bottom: 20),
@@ -119,6 +207,8 @@ class HomePage extends StatelessWidget {
                       ),
                       Divider(color: Colors.grey), // Divider
                       SizedBox(height: 8),
+
+
 
 
                       // Placeholder for upcoming items
@@ -173,57 +263,7 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
 
-                // In-Progress Tasks
-
-                Container(
-                  margin: EdgeInsets.only(bottom: 20),
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(100, 34, 255, 0),
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                      color: Color.fromARGB(100, 34, 255, 0), // 51 is approximately 20% of 255, for similar 20% opacity
-                      blurRadius: 20.0,
-                      spreadRadius: 1.0,
-                      offset: Offset(0, 4),
-                     ),
-                    ],
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'In Progress',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Divider(color: const Color.fromARGB(255, 255, 255, 255)), // Divider
-                      SizedBox(height: 8),
-
-                      // Placeholder for in progress items
-                      Container(
-                        height: 60,
-                        decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Color.fromARGB(139, 255, 255, 255),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.2), // Shadow color with opacity
-                            offset: Offset(0, 4), // Horizontal and vertical offset
-                            blurRadius: 8.0, // Blur radius
-                            spreadRadius: 0, // Spread radius
-                          ),
-                        ],
-                        ),
-                      ),
-
-                    ],
-                  ),
-                ),
+               
 
               ],
             ),
